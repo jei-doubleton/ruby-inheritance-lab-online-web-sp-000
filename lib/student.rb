5eacb7157d953dae357590e6cb28e5a07af322ef
J. Singleton <jennifer.singleton@gmail.com>
2019-02-02 04:02:36 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,4 +6,12 @@ class Student  < User
   def initialize
     @knowledge = []
   end
+
+  def knowledge
+    self.knowledge
+  end
+
+  def learn(knowledge)
+    self.knowledge << knowledge
+  end
 end
